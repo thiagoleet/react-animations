a41168b23c9fda6f0b5eb5fa13f9c7cbb26c7bd5
Thiago Leite <thiago.leite@agileengine.com>
2023-05-30 18:13:36 -0300

    Using a 3rd party lib to add animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,6 +9,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: true,
   };
 
   showModal = () => {
@@ -22,8 +24,48 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop show={this.state.modalIsOpen} />
+
+        {/* <div>
+          <button
+            onClick={() =>
+              this.setState((prevState) => ({
+                showBlock: !prevState.showBlock,
+              }))
+            }
+          >
+            Toggle
+          </button>
+
+          <Transition
+            in={this.state.showBlock}
+            timeout={1000}
+            mountOnEnter
+            unmountOnExit
+          >
+            {(state) => (
+              <div
+                style={{
+                  backgroundColor: "red",
+                  width: "100px",
+                  height: "100px",
+                  margin: "auto",
+                  transition: "opacity 1s ease-out",
+                  opacity: state === "exited" ? 0 : 1,
+                }}
+              ></div>
+            )}
+          </Transition>
+        </div> */}
+
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <Modal show={state} closed={this.closeModal} />}
+        </Transition>
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
